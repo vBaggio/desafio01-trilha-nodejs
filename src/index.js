59c3b372ae73aca4bff34c8b2db8bb614523e302
vBaggio <vbaggio@brainsoft.com.br>
2022-09-26 14:28:29 -0300

    checkExistsUserAccount

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  let { username } = request.headers;
+  let user = users.find((user) => {user.username === username});
+  
+  if (!user) return response.status(400).json({ error: "Customer does not exist" });
+  
+  request.user = user;
+  
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
